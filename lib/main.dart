3ddc38449d09f07d2b7eadf473feb6355e80b56a
PrajwalSugandhi <21ucs152@lnmiit.ac.in>
2023-12-22 04:39:38 +0530

    Camera Functionality #1

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+//import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        initialRoute: '/',
+        routes : {
+          '/': (context) => HomePage(),
+        }
+      )
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //File ? selected_image;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Marks Extraction"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+
+      ),
+
+      body: Center(
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            shape: BoxShape.circle,
+
+          ),
+          child: IconButton(
+
+            icon: Icon(Icons.camera_alt),
+            onPressed: (){
+              _pickImagefromCamera();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future _pickImagefromCamera() async {
+    final returnedimage = await ImagePicker().pickImage(source: ImageSource.camera);
+
+    setState(() {
+      //selected_image = File(returnedimage!.path as List<Object>);
+    });
+  }
+}
+
